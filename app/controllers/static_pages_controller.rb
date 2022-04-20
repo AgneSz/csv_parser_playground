ba72bafd7ab30bdf3dd1d06bb79a73e1cf69f520
AgnieszkaSz <piroskavshofeherke@googlemail.com>
2022-04-20 21:08:11 +0100

    Parsing titles from csv file

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
+require 'csv' 
+
 class StaticPagesController < ApplicationController
   def home
+    # @title = CSV.read("db/netflix_titles.csv")
+    @movies = CSV.foreach(("db/netflix_titles.csv"), headers: true, col_sep: ",") 
   end
 end
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature "User visits home page", :type => :feature do
   it "Displays movies titles" do
     visit "/"
 
-    expect(page).to have_text("Movies titles")
+    expect(page).to have_text("Movie titles")
+    expect(page).to have_text("Dick Johnson Is Dead")
   end
 end
