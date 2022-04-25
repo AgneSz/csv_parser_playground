3bd96d778bb3e0f80647fe07bf4986b937fe807a
Agnieszka Szajnoga <agnieszka.szajnoga@ML-1280.local>
2022-04-25 09:45:06 +0100

    Parse title, director, description details from CSV file

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,15 @@ require 'csv'
 
 class StaticPagesController < ApplicationController
   def home
-    # @title = CSV.read("db/netflix_titles.csv")
+    # CSV.read does not account for headers
+    # CSV.read returns array of arrays; @title[0] => header array;
+    # @title[1] => first row of data, under header, @title[1]["director"] doesn't return any value as it's not a hash
+    @title = CSV.read("db/netflix_titles.csv")
+
+    # CSV.foreach account for headers; @movies.first.class => row; @movies.first.class - CSV::Row
+    # CSV::Row is part an array, part hash;
+    # array: @movies.first[3] => returns value for director key, but accessed by index, not by the key name;
+    # hash: @movies.first["director"] => return value for director key, accessed by key as ususally in hash
     @movies = CSV.foreach(("db/netflix_titles.csv"), headers: true, col_sep: ",") 
   end
 end
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -6,5 +6,7 @@ RSpec.feature "User visits home page", :type => :feature do
 
     expect(page).to have_text("Movie titles")
     expect(page).to have_text("Dick Johnson Is Dead")
+    expect(page).to have_text("Kirsten Johnson")
+    expect(page).to have_text("As her father nears the end of his life")
   end
 end
